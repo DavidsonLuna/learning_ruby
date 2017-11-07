98167807231e26c786e50d6adef3a494df75f6f2
DavidsonLuna <davidsonluna@gmail.com>
2017-11-07 20:38:01 -0200

    new lerning of ruby and rails

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,33 +1,55 @@
-puts "Welcome to the guessing game"
-puts "What's your name?"
-name = gets
-"\n\n\n\n\n\n"
-puts "Loading game for you, " + name
+def welcome
+  puts "Welcome to the guessing game"
+  puts "What's your name?"
+  name = gets
+  "\n\n\n\n\n\n"
+  puts "Loading game for you, " + name
+end
 
-puts "Choosing a secret number between 0 and 200..."
-secret_number = 175
+def draw_secret_number
+  puts "Choosing a secret number between 0 and 200..."
+  drawn = 175
+  puts "Chosen ... how about guessing our secret number today?"
+  drawn
+end
 
-puts "Chosen ... how about guessing our secret number today?"
+def ask_for_a_kick kicks, try, limit_of_try
+  "\n\n\n\n\n\n"
+  puts "Try " + try.to_s + " of " + limit_of_try.to_s
+  puts "Kicks so far: " + kicks.to_s
+  puts "enter the number"
+  kick = gets
+  puts "Did you get it right? You kicked " + kick
+  kick.to_i
+end
 
-limit_of_attempts = 6
-for try in 1..limit_of_attempts
-"\n\n\n\n\n\n"
-puts "Try " + try.to_s + " of " + limit_of_attempts.to_s
-puts "enter the number"
-kick = gets
-puts "Did you get it right? You kicked " + kick
+def try_verify secret_number, kick
+  right = secret_number == kick
 
-right = secret_number == kick.to_i
-
-if right
+  if right
     puts "You're right"
-    break
-  else
-    bigger = secret_number > kick.to_i
-  if bigger
-      puts "The secret number is bigger!"
-    else
-      puts "The secret number is smaller!"
-    end
+    return true
   end
+
+  bigger = secret_number > kick
+  if bigger
+    puts "The secret number is bigger!"
+  else
+    puts "The secret number is smaller!"
+  end
+  false
+end
+
+welcome
+secret_number = draw_secret_number
+
+limit_of_try = 6
+kicks = []
+total_kicks = 0
+
+for try in 1..limit_of_try
+  kick = ask_for_a_kick kicks, try, limit_of_try
+  kicks [total_kicks] = kick
+  total_kicks = total_kicks + 1
+  break if try_verify secret_number,kick
 end
